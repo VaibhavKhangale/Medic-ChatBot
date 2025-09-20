4f111bc5802ff92b2c79f993fe46128c6110a8eb
VaibhavKhangale <vaibhavkhangale15@gmail.com>
2025-09-20 11:06:10 +0530

    env setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medic-ChatBot",
+    version="0.1.0",
+    author="Vaibhav Khangale",
+    author_email="vaibhavkhangale15@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)

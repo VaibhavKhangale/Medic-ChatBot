35c7662a688913c5595066510646418125b2dbea
VaibhavKhangale <vaibhavkhangale15@gmail.com>
2025-09-20 18:29:40 +0530

    project build

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+
+os.environ["PINECONE_API_KEY"]=PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"]=OPENAI_API_KEY
+
+index_name="medic-bot"
+embeddings=download_embeddings()
+
+docsearch=PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriever=docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+chat_model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    api_key="AIzaSyBOQE4BVCmkh8_jCztmFzpJuHtEEI7zj2A",
+    convert_system_message_to_human=True
+)
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system",system_prompt),
+        ("human","{input}"),
+    ]
+)
+
+qna_chain=create_stuff_documents_chain(chat_model, prompt)
+rag_chain=create_retrieval_chain(retriever, qna_chain)
+
+@app.route("/get", methods=["GET","POST"])
+def chat():
+    msg=request.form["msg"]
+    input=msg
+    print(input)
+    response=rag_chain.invoke({"input":msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080, debug=True)
---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+system_prompt = (
+    "You are a helpful medical assistant." 
+    "Answer questions about health, medicine, fitness, and diet clearly and safely." 
+    "Do not give exact diagnoses or prescriptions.When discussing medicines, only explain their general purpose, usage, and side effects — do not prescribe exact dosages unless explicitly provided by official guidelines."
+    "Always remind users to consult a doctor for serious concerns."
+    "Always prioritize safety and evidence-based information. If you do not know answer, ask to consult an expert. Do not Hallucinate."
+    "Answer concisely"
+    "\n\n"
+    "{context}"
+)
